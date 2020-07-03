d87a002ad77136c83ebdea458d5c20f93633b41e
wilinetan <wilinetan26@gmail.com>
2020-07-03 12:03:19 +0800

    add edit account

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -9,6 +9,7 @@ import PasswordForgetPage from "../PasswordForget";
 import HomePage from "../Home";
 import AccountPage from "../Account";
 import AdminPage from "../Admin";
+import EditAccount from "../EditAccount";
 
 import * as ROUTES from "../../constants/routes";
 import { withAuthentication } from "../Session";
@@ -19,7 +20,6 @@ const App = () => (
       <Navigation />
 
       <hr />
-
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
@@ -27,6 +27,7 @@ const App = () => (
       <Route path={ROUTES.HOME} component={HomePage} />
       <Route path={ROUTES.ACCOUNT} component={AccountPage} />
       <Route path={ROUTES.ADMIN} component={AdminPage} />
+      <Route path={ROUTES.EDIT_ACCOUNT} component={EditAccount} />
     </div>
   </Router>
 );
