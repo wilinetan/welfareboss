da9dbf2c97833f67bcc30e3df9e8694082f011d0
jiayi-lim <65161176+jiayi-lim@users.noreply.github.com>
2020-07-22 13:50:10 +0800

    update stats page

--- client/src/components/Statistics/index.js
+++ client/src/components/Statistics/index.js
@@ -1,15 +1,47 @@
 import React from "react";
-
+import {
+	MDBCard,
+	MDBCardBody,
+	MDBCardTitle,
+	MDBCardText,
+	MDBCardGroup,
+	MDBContainer,
+	MDBRow,
+	MDBCol,
+} from "mdbreact";
 import ColDetails from "../Pie";
 import TableDetails from "../Table";
 
 const StatisticsPage = () => {
-  return (
-    <div className="statisticspage" id="statisticspage">
-      <ColDetails />
-      <TableDetails />
-    </div>
-  );
+	return (
+		<div className="statisticspage" id="statisticspage">
+			<MDBContainer>
+				<MDBRow>
+					<MDBCardGroup>
+						<MDBCol md="4">
+							<MDBCard>
+								<MDBCardBody>
+									<MDBCardTitle tag="h5">Pie Chart</MDBCardTitle>
+									<ColDetails />
+								</MDBCardBody>
+							</MDBCard>
+						</MDBCol>
+
+						<MDBCol md="8">
+							<MDBCard>
+								<MDBCardBody data-test="currQueueNum-card">
+									<MDBCardTitle tag="h5" data-test="currQueueNum-cardtitle">
+										Table of collected per hour per day
+									</MDBCardTitle>
+									<TableDetails />
+								</MDBCardBody>
+							</MDBCard>
+						</MDBCol>
+					</MDBCardGroup>
+				</MDBRow>
+			</MDBContainer>
+		</div>
+	);
 };
 
 export default StatisticsPage;
